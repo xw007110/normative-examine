3de5bcd840036531c9d8cb3ccfd5e23bb2c3c088
nanjing-xuanming <565235633@qq.com>
2018-05-28 11:10:12 +0800

    111

---
+++ server/src/main/java/com/wesite/normative/examine/controller/system/UserController.java
@@ -0,0 +1,15 @@
+package com.wesite.normative.examine.controller.system;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 用户Controller
+ *
+ * @author XuanMing
+ * @create 2018/5/16 17:00
+ **/
+@Controller
+@RequestMapping("/user1")
+public class UserController {
+}
